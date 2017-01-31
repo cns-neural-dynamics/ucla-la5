55e342c627e5ff7580f5f082b41d568e3a3111ce
Jessica Dafflon <jejedafflon@gmail.com>
2017-01-31 11:45:25 +0000

    preprocessing: update list of copied reconall files

--- code/merge_reconall_data.py
+++ code/merge_reconall_data.py
@@ -4,16 +4,16 @@ import os
 # subjects_list = ['sub-10290', 'sub-10292', 'sub-10299', 'sub-10304',
 #         'sub-10316', 'sub-10321', 'sub-10325', 'sub-10329', 'sub-10339',
 #         'sub-10340', 'sub-10345', 'sub-10347', 'sub-10356', 'sub-10361',
-#         'sub-10365', 'sub-10376', 'sub-10377', 'sub-10388']
-
-subjects_list = ['sub-50004', 'sub-50005', 'sub-50006', 'sub-50007', 'sub-50008',
-    'sub-50010', 'sub-50013', 'sub-50014', 'sub-50015', 'sub-50016',
-    'sub-50020', 'sub-50021', 'sub-50022', 'sub-50023', 'sub-50025',
-    'sub-50027', 'sub-50029', 'sub-50032', 'sub-50033', 'sub-50034',
-    'sub-50035', 'sub-50036', 'sub-50038', 'sub-50060', 'sub-50061',
-    'sub-50064', 'sub-50066', 'sub-50067', 'sub-50069', 'sub-50073',
-    'sub-50075', 'sub-50076', 'sub-50077', 'sub-50080', 'sub-50081',
-    'sub-50083', 'sub-50085']
+#         'sub-10365', 'sub-10376', 'sub-10377', 'sub-10388', 'sub-50004',
+#         'sub-50005', 'sub-50006', 'sub-50007', 'sub-50008', 'sub-50010',
+#         'sub-50013', 'sub-50014', 'sub-50015', 'sub-50016', 'sub-50020',
+#         'sub-50021', 'sub-50022', 'sub-50023', 'sub-50025', 'sub-50027',
+#         'sub-50029', 'sub-50032', 'sub-50033', 'sub-50034', 'sub-50035',
+#         'sub-50036', 'sub-50038', 'sub-50060', 'sub-50061', 'sub-50064',
+#         'sub-50066', 'sub-50067', 'sub-50069', 'sub-50073', 'sub-50075',
+#         'sub-50076', 'sub-50077', 'sub-50080', 'sub-50081', 'sub-50083',
+#         'sub-50085']
+subjects_list = ['sub-10429', 'sub-10438', 'sub-10440']
 base_data = '/home/jdafflon/scratch/personal/data_in/ucla_la5/ds000030'
 for sub in subjects_list:
     anat_data = os.path.join(base_data, sub, 'anat')
