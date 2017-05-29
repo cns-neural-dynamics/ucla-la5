5346bea6fe8a7074ff2a9b2d1cef570861039963
Jessica Dafflon <jejedafflon@gmail.com>
2017-05-29 18:22:14 +0100

    Add script that call analysis

---
+++ code/analysis.sh
@@ -0,0 +1,5 @@
+###############################################################################
+# Golden Subjects
+###############################################################################
+# Perform ROI extraction and calculate dynamic measures for the golden subjects
+python main_analysis.py -n 1 -r -a -c --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network  --ica_aroma-type nonaggr --glm_denoise  --nclusters 10 --rand-ind 20 --group-analysis-type ttest
