8ec41ce21fd66e4cf08e79c35e2239f95169db32
Jessica Dafflon <jessica.dafflon@gmail.com>
2017-06-03 15:18:46 +0100

    bash_analysis: intelligent bash script
    * submit golden analysis for 3<ncluster<10
    * sumit analysis for 3<ncluster<10

--- code/analysis.sh
+++ code/analysis.sh
@@ -1,106 +1,24 @@
 #!/usr/bin/env bash
 source activate ucla-la5
 
-###############################################################################
-#                           ncluster = 3
-###############################################################################
+allclusters=(3 4 5 6 7 8 9 10)
+tasktype=rest
 
-# Golden Subjects
-###############################################################################
-# Perform ROI extraction and calculate dynamic measures for the golden subjects
-tmux new-session -d -s cluster3_gs 'python main_analysis.py -n 1 -r -a -c --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 3 --rand-ind 20 --group-analysis-type ttest'
+for i in "${allclusters[@]}"
+do
+    tmux new-session -d -s cluster"$i"_gs 'python main_analysis.py -n 1 -r -a -c --analysis-type '"$tasktype"' --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise --nclusters '"$i"' --rand-ind 20 --group-analysis-type ttest'
+done
 
-sleep 5s
-# Group Analysis (roi extraction, analysis and group analysis)
-###############################################################################
-tmux new-session -d -s cluster3 'python main_analysis.py -n 20 -a -g --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 3 --rand-ind 20 --group-analysis-type ttest'
+# wait until the last of the job for golden subjects has been submitted
+#$? corresponds to the output from the previous commit
+tmux has-session -t cluster"${allclusters[-1]}"_gs
+while [ $? -ne 0 ];
+do
+    sleep 10s
+done
 
-sleep 5s
-###############################################################################
-#                           ncluster = 4
-###############################################################################
-
-# Golden Subjects
-###############################################################################
-# Perform ROI extraction and calculate dynamic measures for the golden subjects
-tmux new-session -d -s cluster4_gs 'python main_analysis.py -n 1 -r -a -c --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 4 --rand-ind 20 --group-analysis-type ttest'
-
-sleep 5s
-# Group Analysis (roi extraction, analysis and group analysis)
-###############################################################################
-tmux new-session -d -s cluster4 'python main_analysis.py -n 20 -a -r -g --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 4 --rand-ind 20 --group-analysis-type ttest'
-
-sleep 5s
-###############################################################################
-#                           ncluster = 5
-###############################################################################
-
-# Golden Subjects
-###############################################################################
-# Perform ROI extraction and calculate dynamic measures for the golden subjects
-tmux new-session -d -s cluster5_gs 'python main_analysis.py -n 1 -r -a -c --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 5 --rand-ind 20 --group-analysis-type ttest'
-
-sleep 5s
-# Group Analysis (roi extraction, analysis and group analysis)
-###############################################################################
-tmux new-session -d -s cluster5 'python main_analysis.py -n 20 -a -r -g --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 5 --rand-ind 20 --group-analysis-type ttest'
-
-sleep 5s
-
-###############################################################################
-#                           ncluster = 6
-###############################################################################
-
-# Golden Subjects
-###############################################################################
-# Perform ROI extraction and calculate dynamic measures for the golden subjects
-tmux new-session -d -s cluster6_gs 'python main_analysis.py -n 1 -a -c --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 6 --rand-ind 20 --group-analysis-type ttest'
-
-sleep 5s
-# Group Analysis (roi extraction, analysis and group analysis)
-###############################################################################
-tmux new-session -d -s cluster6 'python main_analysis.py -n 20 -a -g --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 6 --rand-ind 20 --group-analysis-type ttest'
-
-sleep 5s
-###############################################################################
-#                           ncluster = 7
-###############################################################################
-
-# Golden Subjects
-###############################################################################
-# Perform ROI extraction and calculate dynamic measures for the golden subjects
-tmux new-session -d -s cluster7_gs 'python main_analysis.py -n 1  -a -c --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 7 --rand-ind 20 --group-analysis-type ttest'
-
-sleep 5s
-# Group Analysis (roi extraction, analysis and group analysis)
-###############################################################################
-tmux new-session -d -s cluster7 'python main_analysis.py -n 20 -a -r -g --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 7 --rand-ind 20 --group-analysis-type ttest'
-
-sleep 5s
-###############################################################################
-#                           ncluster = 8
-###############################################################################
-
-# Golden Subjects
-###############################################################################
-# Perform ROI extraction and calculate dynamic measures for the golden subjects
-tmux new-session -d -s cluster8_gs 'python main_analysis.py -n 1  -a -c --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 8 --rand-ind 20 --group-analysis-type ttest'
-
-sleep 5s
-# Group Analysis (roi extraction, analysis and group analysis)
-###############################################################################
-tmux new-session -d -s cluster8 'python main_analysis.py -n 20 -a -g --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 8 --rand-ind 20 --group-analysis-type ttest'
-
-sleep 5s
-#############################################################################
-                          ncluster = 10
-#############################################################################
-
-Golden Subjects
-#############################################################################
-Perform ROI extraction and calculate dynamic measures for the golden subjects
-python main_analysis.py -n 1 -r -a -c --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 10 --rand-ind 20 --group-analysis-type ttest
-
-# Group Analysis
-###############################################################################
-tmux new-session -d -s cluster10 'python main_analysis.py -n 20 -a -g --analysis-type task --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise  --nclusters 10 --rand-ind 20 --group-analysis-type ttest'
+echo "submitting analysis"
+for i in "${allclusters[@]}"
+do
+    tmux new-session -d -s cluster"$i" 'python main_analysis.py -n 20 -a -g --analysis-type '"$tasktype"' --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise --nclusters '"$i"' --rand-ind 20 --group-analysis-type ttest'
+done
