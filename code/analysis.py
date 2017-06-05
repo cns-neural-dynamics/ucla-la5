bd7ab37a46920a9fd3c83562e881a2471930b5e3
Jessica Dafflon <jejedafflon@gmail.com>
2017-06-05 12:07:09 +0100

    analysis tmux
    * iterate over task and ret and submit analysis for all clusters

--- code/analysis.py
+++ code/analysis.py
@@ -3,28 +3,32 @@ import libtmux
 import time
 
 
-all_clusters = ['3', '4', '5', '6', '7', '8', '9', '10']
-tasktype='rest'
+clusters = ['3', '4', '5', '6', '7', '8', '9', '10']
+tasktypes = ['rest', 'task']
 server = libtmux.Server()
 
-for ncluster in all_clusters:
-    cmd1 = "tmux new-session -d -s cluster%s_gs 'python main_analysis.py -n 1 -r -a -c --analysis-type %s --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise --nclusters %s --rand-ind 20 --group-analysis-type ttest'" \
-          %(ncluster, tasktype, ncluster)
-    print cmd1
-    subprocess.check_call(cmd1, shell=True)
-    curr_gs = ''.join(['cluster', ncluster, '_gs'])
-    while server.find_where({'session_name':curr_gs}):
-        time.sleep(15)
+# Note: because of the way find_where works, if there are no tmux windows the code will fail. Thefore, we create a dummy
+# tmux that will be present throught the code and killed at the end
+dummy_tmux_cmd = "tmux new-session -d -s dummy"
+subprocess.call(dummy_tmux_cmd, shell=True)
 
-# check if golden subjects is still runing, in case yes, wait
-last_gs = ''.join(['cluster', all_clusters[-1], '_gs'])
-while server.find_where({'session_name':last_gs}):
-    time.sleep(15)
+for tasktype in tasktypes:
+    for cluster in clusters:
+        cmd1 = "tmux new-session -d -s cluster%s_gs1 'python main_analysis.py -n 1 -r -a -c --analysis-type %s --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise --nclusters %s --rand-ind 20 --group-analysis-type ttest'" \
+              %(cluster, tasktype, cluster)
+        print cmd1
+        subprocess.call(cmd1, shell=True)
+        curr_gs = ''.join(['cluster', cluster, '_gs1'])
+        while server.find_where({'session_name':curr_gs}):
+            time.sleep(15)
 
-for ncluster in all_clusters:
-    cmd2 = "tmux new-session -d -s cluster%s 'python main_analysis.py -n 20 -r -a -g --analysis-type %s --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise --nclusters %s --rand-ind 20 --group-analysis-type ttest'" \
-           %(ncluster, tasktype, ncluster)
-    subprocess.check_output(cmd2, shell=True)
-    print cmd2
+for tasktype in tasktypes:
+    for cluster in clusters:
+        cmd2 = "tmux new-session -d -s cluster%s 'python main_analysis.py -n 20 -r -a -g --analysis-type %s --data-analysis-type graph_analysis --window-type sliding --network-type full_network --ica_aroma-type nonaggr --glm_denoise --nclusters %s --rand-ind 20 --group-analysis-type ttest'" \
+               %(cluster, tasktype, cluster)
+        subprocess.call(cmd2, shell=True)
+        print cmd2
 
-
+# kill dummy tmux
+kill_dummy = "tmux kill-session -t dummy"
+subprocess.check_output(kill_dummy, shell=True)
